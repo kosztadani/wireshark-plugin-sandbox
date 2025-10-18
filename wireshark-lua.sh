a49351f7ce60709bbce693bd32a684b6bdd66bff
Dániel Koszta <daniel.koszta@gmail.com>
2025-10-18 12:04:41 +0200

    Allow overriding Wireshark path in start scripts
    Change-Id: I8425f2990bd4939c1f9f3c98ab789b9ad0a9611a

--- wireshark-lua.sh
+++ wireshark-lua.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
+WIRESHARK="${WIRESHARK:-wireshark}"
+
 repository="$(readlink -f "$(dirname "${0}")")"
 export WIRESHARK_CONFIG_DIR="${repository}/config"
 
-exec wireshark \
+exec "${WIRESHARK}" \
     -X lua_script:"${repository}/my-math.lua" \
     "$@"
--- wireshark-native.sh
+++ wireshark-native.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+WIRESHARK="${WIRESHARK:-wireshark}"
+
 repository="$(readlink -f "$(dirname "${0}")")"
 export WIRESHARK_CONFIG_DIR="${repository}/config"
 export WIRESHARK_PLUGIN_DIR="${repository}/config/my-plugins"
 
-exec wireshark "$@"
+exec "${WIRESHARK}" "$@"
